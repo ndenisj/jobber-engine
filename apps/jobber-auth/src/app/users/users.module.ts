ff114e3a8a89ddbfa8d8163a51cc07f48e67778c
Denis John <denis.john.ndarake@gmail.com>
2025-02-13 19:01:26 +0100

    implement user creation and retrieval functionality with validation

--- apps/jobber-auth/src/app/users/users.module.ts
+++ apps/jobber-auth/src/app/users/users.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+    imports: [
+        PrismaModule
+    ],
     providers: [
         UsersResolver, UsersService
     ]
--- apps/jobber-auth/src/app/users/users.resolver.ts
+++ apps/jobber-auth/src/app/users/users.resolver.ts
@@ -1,11 +1,21 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './models/user.model';
 import { UsersService } from './users.service';
+import { CreateUserInput } from './dto/create-user.input';
 
 @Resolver(() => User)
 export class UsersResolver {
     constructor(private readonly usersService: UsersService) { }
 
+    @Mutation(() => User, { name: 'createUser' })
+    async createUser(
+        @Args('createUserInput') createUserInput: CreateUserInput
+    ) {
+        return await this.usersService.createUser(createUserInput);
+    }
+
     @Query(() => [User], { name: 'users' })
-    async getUsers() { }
+    async getUsers() {
+        return await this.usersService.getUsers();
+    }
 }
