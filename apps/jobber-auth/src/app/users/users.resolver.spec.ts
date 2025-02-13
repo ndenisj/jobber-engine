9fdd0975183e2fb8aaf7c76a6e956bf8f37a8175
Denis John <denis.john.ndarake@gmail.com>
2025-02-13 09:03:35 +0100

    initialize jobber-auth application with NestJS and PostgreSQL setup, and setup graphql

---
+++ apps/jobber-auth/src/app/users/users.resolver.spec.ts
@@ -0,0 +1,18 @@
+import { UsersResolver } from './users.resolver';
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('UsersResolver', () => {
+  let resolver: UsersResolver;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UsersResolver],
+    }).compile();
+
+    resolver = module.get<UsersResolver>(UsersResolver);
+  });
+
+  it('should be defined', () => {
+    expect(resolver).toBeDefined();
+  });
+});
