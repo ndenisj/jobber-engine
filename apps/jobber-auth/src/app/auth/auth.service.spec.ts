b3a7ef2abc9c9b289b071a3ab1173cd8e59b1011
Denis John <denis.john.ndarake@gmail.com>
2025-02-14 05:05:41 +0100

    feat: implement authentication module with login functionality and JWT support

---
+++ apps/jobber-auth/src/app/auth/auth.service.spec.ts
@@ -0,0 +1,35 @@
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from '../users/users.service';
+import { AuthService } from './auth.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { JwtService } from '@nestjs/jwt';
+
+describe('AuthService', () => {
+  let service: AuthService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: {},
+        },
+        {
+          provide: ConfigService,
+          useValue: {},
+        },
+        {
+          provide: JwtService,
+          useValue: {},
+        },
+      ],
+    }).compile();
+
+    service = module.get<AuthService>(AuthService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
