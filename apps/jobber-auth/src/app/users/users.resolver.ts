7cc8684e988735af8d105bcb7e21d2dd90526b8d
Denis John <denis.john.ndarake@gmail.com>
2025-02-15 02:31:30 +0100

    feat: add JWT authentication strategy and GraphQL guard

--- apps/jobber-auth/src/app/users/users.resolver.ts
+++ apps/jobber-auth/src/app/users/users.resolver.ts
@@ -2,20 +2,21 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './models/user.model';
 import { UsersService } from './users.service';
 import { CreateUserInput } from './dto/create-user.input';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from '../auth/guards/gql-auth.guard';
 
 @Resolver(() => User)
 export class UsersResolver {
-    constructor(private readonly usersService: UsersService) { }
+  constructor(private readonly usersService: UsersService) {}
 
-    @Mutation(() => User, { name: 'createUser' })
-    async createUser(
-        @Args('createUserInput') createUserInput: CreateUserInput
-    ) {
-        return await this.usersService.createUser(createUserInput);
-    }
+  @Mutation(() => User, { name: 'createUser' })
+  async createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
+    return await this.usersService.createUser(createUserInput);
+  }
 
-    @Query(() => [User], { name: 'users' })
-    async getUsers() {
-        return await this.usersService.getUsers();
-    }
+  @UseGuards(GqlAuthGuard)
+  @Query(() => [User], { name: 'users' })
+  async getUsers() {
+    return await this.usersService.getUsers();
+  }
 }
